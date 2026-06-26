df99f18bfe89d800d7e7c86eef31d8f5f3abf4c6
Mayowa Rosanwo <thenextwaveapp@gmail.com>
2026-06-26 12:23:44 -0600

    Add phone to Paystack shipping address interface

--- src/lib/paystack.ts
+++ src/lib/paystack.ts
@@ -18,6 +18,7 @@ export interface PaystackCheckoutRequest {
   shippingAddress: {
     fullName: string;
     email: string;
+    phone: string;
     address: string;
     city: string;
     state: string;
